b17805ca2b2ea31653c8a89346c6a0baaf34c12f
Patrick Kottwitz <patrickkottwitz@gmail.com>
2023-09-09 22:56:30 -0300

    adicionado faker e utilizado para alimentar os templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # def home(request):
 #     return HttpResponse('home')
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Patrick'
+        'recipes': [make_recipe() for _ in range(13)],
     })
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
-        'name': 'Patrick'
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
     })
